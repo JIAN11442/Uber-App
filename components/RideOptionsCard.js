ad62e518f84539b5467b88d494e6fc94d2eaaeff
JIAN11442 <yikjian11442@gmail.com>
2023-07-11 17:50:15 +0800

    Get TravelTime With DistanceMatrix

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -8,12 +8,25 @@ import { useEffect } from "react";
 import sanityClient, { urlFor } from "../sanity";
 import { useDispatch, useSelector } from "react-redux";
 import { selectRideCarTypes, setRideCarType } from "../feature/useStateSlice";
+import {
+  selectDestination,
+  selectOrigin,
+  selectTravelTimeInformation,
+  setTravelTimeInformation,
+} from "../feature/navSlice";
+import { GOOGLE_MAPS_APIKEYS } from "@env";
+import Currency from "react-currency-formatter";
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const rideCarTypes = useSelector(selectRideCarTypes);
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
   const [isSelected, setIsSelected] = useState("");
+  const SURGE_PRICING_RATE = 1.5;
+  const PRICES_PER_VALUE = 8;
 
   useEffect(() => {
     sanityClient
@@ -30,17 +43,34 @@ const RideOptionsCard = () => {
   }, []);
 
   useEffect(() => {
-    if (isSelected !== "") {
-      console.log(isSelected);
-      console.log(`------------------`);
-    }
-  }, [isSelected]);
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+      // https://developers.google.com/maps/documentation/distance-matrix/start#json
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?
+        units=metric&origins=${origin.description}&destinations=
+        ${destination.description}&key=${GOOGLE_MAPS_APIKEYS}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEYS]);
+
+  useEffect(() => {
+    console.log(travelTimeInformation);
+    console.log(`-------------------------`);
+  }, [travelTimeInformation]);
 
   return (
-    <View>
+    <View style={tw`flex-1 bg-white`}>
       {/* Welcome Box */}
       <View style={tw`p-3 bg-white border-b border-gray-200 shadow-sm`}>
-        <Text style={tw`text-lg text-center font-bold`}>Select a Ride</Text>
+        <Text style={tw`text-lg text-center font-semibold`}>Select a Ride</Text>
         <TouchableOpacity
           style={tw`absolute top-4 left-6`}
           onPress={() => navigation.goBack()}
@@ -54,50 +84,69 @@ const RideOptionsCard = () => {
         </TouchableOpacity>
       </View>
       {/* Ride Type Options */}
-      <View style={tw`flex bg-white h-full`}>
-        <FlatList
-          data={rideCarTypes}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => {
-                setIsSelected(item);
-              }}
-              style={tw`${isSelected._id === item._id ? `bg-gray-100` : ``}`}
+      <FlatList
+        data={rideCarTypes}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              setIsSelected(item);
+            }}
+            style={tw`${isSelected._id === item._id ? `bg-gray-200` : ``}`}
+          >
+            <View
+              style={tw`flex-row items-center justify-between 
+                  mr-5 ml-2 px-4 border-b border-gray-100`}
             >
-              <View
-                style={tw`flex-row items-center justify-between 
-                  mx-5 px-5 border-b border-gray-100`}
-              >
-                {/* Ride Image */}
-                <Image
-                  style={{
-                    bottom: 10,
-                    width: 100,
-                    height: 90,
-                    resizeMode: "contain",
-                  }}
-                  source={{
-                    uri: urlFor(item.rideImage).url(),
-                  }}
-                />
-                {/* Ride Name & Travel Time */}
-                <View style={tw`-ml-6`}>
-                  <Text style={tw`text-lg font-semibold`}>
-                    {item.rideCarsType}
-                  </Text>
-                  <Text>Travel Time...</Text>
-                </View>
-                {/* Prices */}
-                <View>
-                  <Text style={tw`text-lg`}>$99</Text>
-                </View>
+              {/* Ride Image */}
+              <Image
+                style={{
+                  bottom: 10,
+                  width: 100,
+                  height: 90,
+                  resizeMode: "contain",
+                }}
+                source={{
+                  uri: urlFor(item.rideImage).url(),
+                }}
+              />
+              {/* Ride Name & Travel Time */}
+              <View style={tw`w-35`}>
+                <Text style={tw`text-xl font-semibold`}>
+                  {item.rideCarsType}
+                </Text>
+                <Text>{travelTimeInformation?.duration.text} Travel Time</Text>
               </View>
-            </TouchableOpacity>
-          )}
-        />
-
-        <FlatList />
+              {/* Prices */}
+              <View>
+                <Text style={tw`text-lg font-semibold`}>
+                  <Currency
+                    quantity={
+                      (travelTimeInformation?.duration?.value *
+                        item.multiplier *
+                        SURGE_PRICING_RATE) /
+                      PRICES_PER_VALUE
+                    }
+                    currency="TWD"
+                  />
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+      <FlatList />
+      {/* Choosed Ride Type Button */}
+      <View
+        style={tw` py-3 mx-8 my-4 rounded-md ${
+          !isSelected ? `bg-gray-300` : `bg-black`
+        }`}
+      >
+        <TouchableOpacity disabled={!isSelected}>
+          <Text style={tw`text-lg text-white font-semibold text-center`}>
+            Choose {isSelected?.rideCarsType}
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
